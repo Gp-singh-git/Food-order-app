8deb400b203f7174db8891080e54758c09944613
gurpreet singh <gurpreet212004@gmail.com>
2021-08-22 01:04:16 +0000

    Schema added

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,58 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS dishes CASCADE;
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  mobile_number INT(10) NOT NULL,
+  email VARCHAR(255),
+  password VARCHAR(255) NOT NULL,
+  address VARCHAR(255)
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  total_price INT,
+  order_date date,
+  order_time = time,
+  ready_by = time,
+  picked_at = time,
+);
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  rating SMALLINT,
+  feedback TEXT
+);
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100),
+  description TEXT,
+  image TEXT,
+  price INTEGER
+);
+
+CREATE TABLE ordered_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  dish_id INTEGER REFERENCES dishes(id) ON DELETE CASCADE,
+  quantity SMALLINT
+);
+
+CREATE TABLE cart(
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  dish_id INTEGER REFERENCES dishes(id) ON DELETE CASCADE,
+  quantity SMALLINT
+
 );
