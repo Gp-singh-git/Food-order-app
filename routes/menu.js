ca2c8ea08510e089221c73888a9ebc22750e1960
gurpreet singh <gurpreet212004@gmail.com>
2021-08-26 21:43:43 +0000

    add comments and eslint files remove dead code

--- routes/menu.js
+++ routes/menu.js
@@ -1,29 +1,7 @@
-/*
- * All routes for Menus are defined here
- * Since this file is loaded in server.js into menu,
- *   these routes are mounted onto /menu
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  // router.get("/", (req, res) => {
-  //   let query = `SELECT * FROM items`;
-  //   console.log(query);
-  //   db.query(query)
-  //     .then(data => {
-  //       const menu = data.rows;
-  //       res.json({ menu });
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
 
   // ----- gets all menu items ---- //
 
@@ -46,34 +24,5 @@ module.exports = (db) => {
       });
   });
 
-  // router.get("/users", (req,res) => {
-  //   db.query(`
-  //   SELECT *
-  //   FROM users;
-  //   `)
-  //     .then(data => {
-  //       console.log("my function is running")
-  //       const users = data.rows;
-  //       res.json({ users });
-  //     })
-  //         .catch(err => {
-  //         res
-  //           .status(500)
-  //           .json({ error: err.message });
-  //       });
-  // });
-
   return router;
 };
-
-
-//   // ----- add a single menu item to the db ----- //
-
-//   const addMenuItem = function (menuItem) {
-//     return db.query(`
-//     INSERT INTO items (
-//       name, description, image, price)
-//       VALUES ($1, $2, $3, $4) RETURNING *;
-//     `, [menuItem.name, menuItem.description, menuItem.image, menuItem.price])
-//       .then(res => res.rows[0]);
-//   };
--- routes/owner_orders.js
+++ routes/owner_orders.js
@@ -5,28 +5,25 @@ module.exports = (db, client) => {
   router.get("/", (req, res) => {
     const ckv_id = req.session.ck_id;
     const ckv_fn = req.session.ck_fn;
-
+//displaying all items received. tables users and orders
     db.query(`SELECT users.first_name, users.last_name, orders.id, orders.order_time, orders.ready_by, orders.picked_at from orders JOIN users ON orders.user_id = users.id ORDER BY id;
     `)
       .then(data => {
         const items = data.rows;
-        const promises=[];
-        for (let i=0; i<items.length; i++) {
+        const promises = [];
+        for (let i = 0; i < items.length; i++) {
           promises.push(db.query(`SELECT items.name, ordered_items.quantity from ordered_items JOIN items on ordered_items.item_id = items.id where order_id = ${items[i]["id"]};`));
-
+//tables: ordered items and items
         }
-        Promise.all(promises)
-        .then(values => {
-          // console.log(values);
-          for (let i=0; i<items.length; i++) {
+        Promise.all(promises)           //appending order details against respective order id
+          .then(values => {
+            for (let i = 0; i < items.length; i++) {
               items[i]["details"] = values[i].rows;
-          }
-          res.render("owner_orders", {items, ckv_id, ckv_fn})
+            }
+            res.render("owner_orders", {items, ckv_id, ckv_fn});
 
-        })
-      // console.log(items);
-
-    })
+          });
+      })
       .catch(err => {
         res
           .status(500)
@@ -34,34 +31,35 @@ module.exports = (db, client) => {
       });
   });
 
-
+//To update ready value in orders table
   router.post("/:tm", (req,res) => {
     db.query(`UPDATE orders SET ready_by = '${req.body.ready_value}' WHERE id = ${req.params.tm}`)
-    .then( () => {
-      res.status(200);
-      db.query(`SELECT first_name, mobile_number from users JOIN orders ON users.id = orders.user_id where orders.id = ${req.params.tm};`)
-      .then(datax => {
-        const j = datax.rows[0];
-        console.log("mobilllllll---------------------",j.mobile_number);
-      client.messages.create({
-        to: `${j.mobile_number}`,
-        from: '+14083407572',
-        body: `Dear ${j.first_name}, Your order no. ${req.params.tm} received with Royal Bakery will be ready by ${req.body.ready_value}.`})
-      .then(message => console.log(message))
-      .catch(error => console.log(error));
-      })
-      res.redirect('/ownerorder');
-    });
+      .then(() => {
+        res.status(200);
+        db.query(`SELECT first_name, mobile_number from users JOIN orders ON users.id = orders.user_id where orders.id = ${req.params.tm};`)
+          .then(datax => {
+            const j = datax.rows[0];
+
+  // Sending SMS to Customer when order will be ready
+            client.messages.create({
+              to: `${j.mobile_number}`,
+              from: '+14083407572',
+              body: `Dear ${j.first_name}, Your order no. ${req.params.tm} received with Royal Bakery will be ready by ${req.body.ready_value}.`})
+              .then(message => console.log(message))
+              .catch(error => console.log(error));
+          });
+        res.redirect('/ownerorder');
+      });
   });
 
-
+//To update picked at time on orders table
   router.post("/u/:pk", (req,res) => {
-  console.log("recd val: ",req.body.picked_value,"params pk: ",req.params.pk );
-  db.query(`UPDATE orders SET picked_at = '${req.body.picked_value}' WHERE id = ${req.params.pk}`)
-  .then( () => {
-    res.redirect('/ownerorder');
-  })
-  })
+    console.log("recd val: ",req.body.picked_value,"params pk: ",req.params.pk);
+    db.query(`UPDATE orders SET picked_at = '${req.body.picked_value}' WHERE id = ${req.params.pk}`)
+      .then(() => {
+        res.redirect('/ownerorder');
+      });
+  });
 
-return router;
-}
+  return router;
+};
